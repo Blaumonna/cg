2f3fcca087f1420d20f6128635ff73f5c12e933c
Kamilla Gusmanova <blaumonna@gmail.com>
2022-04-02 15:28:15 +0500

    Point

--- cg/cg.cpp
+++ cg/cg.cpp
@@ -1,8 +1,20 @@
+#include <stdio.h>
+#include <GL/glew.h>
 #include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include "math_3d.h"
 
+GLuint VBO;
 static void RenderSceneCB()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glDrawArrays(GL_POINTS, 0, 1);
+
+	glDisableVertexAttribArray(0);
 	glutSwapBuffers();
 }
 
@@ -11,6 +23,14 @@ static void InitializeGlutCallbacks()
 	glutDisplayFunc(RenderSceneCB);
 }
 
+static void CreateVertex() 
+{
+	Vector3f Vertices[1];
+	Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
 
 int main(int argc, char** argv)
 {
@@ -22,7 +42,17 @@ int main(int argc, char** argv)
 
 	InitializeGlutCallbacks();
 
-	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		return 1;
+	}
+
+
+	glClearColor(0.8f, 0.2f, 0.2f, 0.0f);
+
+	CreateVertex();
 
 	glutMainLoop();
 
