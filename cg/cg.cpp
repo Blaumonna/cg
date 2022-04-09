d46df9ff467c7be7663c97bfcb79e63aaf837956
Kamilla Gusmanova <blaumonna@gmail.com>
2022-04-09 14:46:01 +0500

    Scaling

--- cg/cg.cpp
+++ cg/cg.cpp
@@ -32,9 +32,9 @@ static void RenderSceneCB()
 	static float Scale = 0.0f;
 	Scale += 0.001f;
 	glm::mat4 Matrix; 
-	Matrix[0][0] = cosf(Scale); Matrix[0][1] = -sinf(Scale); Matrix[0][2] = 0.0f; Matrix[0][3] = 0.0f;
-	Matrix[1][0] = sinf(Scale); Matrix[1][1] = cosf(Scale); Matrix[1][2] = 0.0f; Matrix[1][3] = 0.0f;
-	Matrix[2][0] = 0.0f; Matrix[2][1] = 0.0f; Matrix[2][2] = 1.0f; Matrix[2][3] = 0.0f;
+	Matrix[0][0] = sinf(Scale); Matrix[0][1] = 0.0f; Matrix[0][2] = 0.0f; Matrix[0][3] = 0.0f;
+	Matrix[1][0] = 0.0f; Matrix[1][1] = cosf(Scale); Matrix[1][2] = 0.0f; Matrix[1][3] = 0.0f;
+	Matrix[2][0] = 0.0f; Matrix[2][1] = 0.0f; Matrix[2][2] = sinf(Scale); Matrix[2][3] = 0.0f;
 	Matrix[3][0] = 0.0f; Matrix[3][1] = 0.0f; Matrix[3][2] = 0.0f; Matrix[3][3] = 1.0f;
 	glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &Matrix[0][0]);
 
